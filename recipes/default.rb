e1ffbc7aff6dffafdf3e2beef051b4f3f653765a
Adam Staudt <astaudt@shutterstock.com>
2014-09-08 01:43:21 +0200

    offload a bunch of processing to forked recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,33 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-hostname = "sstk-wp"
+hostname = "blog.shutterstock.com"
 
-package 'ntp'
-
-service 'ntpd' do
-  supports :status => true, :restart => true, :reload => true
-  action [ :enable, :start ]
-end
+package "git"
 
 include_recipe "build-essential"
 include_recipe "env_vars"
 include_recipe "iptables::disabled"
-include_recipe "nginx::source"
-
-[
-  "#{node['nginx']['dir']}/vhost.d",
-  "#{node['nginx']['log_dir']}/vhost",
-  "/var/www/wp"
-].each do |dir|
-  directory "#{dir}" do
-  owner node['nginx']['user']
-  group node['nginx']['group']
-  mode 0755
-  recursive true
-  action :create
-  end
-end
+include_recipe "wordpress::nginx"
 
 template "#{node['nginx']['dir']}/nginx.conf" do
   source "nginx.conf.erb"
@@ -42,24 +23,6 @@ template "#{node['nginx']['dir']}/nginx.conf" do
   action :create
   notifies :reload, 'service[nginx]'
 end
-
-template "#{node['nginx']['dir']}/vhost.d/#{hostname}.conf" do
-  source "#{hostname}.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  action :create
-  notifies :reload, 'service[nginx]'
-end
-
-cookbook_file "/var/www/wp/index.html" do
-  source "index.html"
-  owner node['nginx']['user']
-  group node['nginx']['group']
-  mode 0644
-  action :create
-end
-
 
 mysql_service 'default' do
   server_root_password node['mysql']['server_root_password']
