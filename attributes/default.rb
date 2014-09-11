4a4a0a1630f2fa54f96137b792b9c4e82273116a
Adam Staudt <astaudt@shutterstock.com>
2014-09-11 14:41:00 +0200

    move attributes to wordpress cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -21,8 +21,6 @@ default['nginx']['client_max_body_size'] = '100m'
 
 # wordpress attributes
 default['wordpress']['version'] = 'latest'
-default['wordpress']['db']['user'] = "sstkdbadmin"
-default['wordpress']['db']['pass'] = 'C>+{9&TYuVme3HHxLpxa+R'
 default['wordpress']['install']['user'] = node['nginx']['user']
 default['wordpress']['install']['group'] = node['nginx']['group']
 default['wordpress']['allow_multisite'] = true
