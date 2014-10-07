d907ea49129a720e0f24876ca1373a9f871cca09
Adam Staudt <astaudt@shutterstock.com>
2014-10-07 12:26:16 +0200

    bump chefdk version

--- utils/init.sh
+++ utils/init.sh
@@ -2,7 +2,7 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/opt/chefdk/embedded/bin:/opt/chefdk/bin
 
-CHEFDK_URL="https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.2.1-1.el6.x86_64.rpm"
+CHEFDK_URL="https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.3.0-1.x86_64.rpm"
 SSTK_WP_COOKBOOK_URL="https://github.shuttercorp.net/astaudt/chef-sstk-wp.git"
 
 rpm -Uvh $CHEFDK_URL
