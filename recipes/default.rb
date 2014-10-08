4b707c373843b64457231c5206ef9ee3865cc42e
Adam Staudt <astaudt@shutterstock.com>
2014-10-08 13:24:16 +0200

    change order of operation

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,25 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# set the hostname
 hostname = "blog.shutterstock.com"
-
-%w( git telnet nc findutils mlocate ).each do |pkg|
-  package pkg
-end
-
-include_recipe "build-essential"
-include_recipe "env_vars"
-include_recipe "iptables::disabled"
-include_recipe "wordpress::nginx"
-
-template "#{node['nginx']['dir']}/nginx.conf" do
-  source "nginx.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  action :create
-  notifies :reload, 'service[nginx]'
-end
 
 file '/etc/hostname' do
   content "#{hostname}\n"
@@ -36,5 +20,29 @@ bash "set hostname to #{hostname}" do
   EOH
 end
 
+# install a few packages we need
+%w( git telnet nc findutils mlocate ).each do |pkg|
+  package pkg
+end
+
+# pull in recipes to do some scaffolding work and install/configure wordpress
+include_recipe "build-essential"
+include_recipe "env_vars"
+include_recipe "iptables::disabled"
+include_recipe "wordpress::nginx"
+
+# write the true nginx config
+template "#{node['nginx']['dir']}/nginx.conf" do
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  action :create
+  notifies :reload, 'service[nginx]'
+end
+
 # this has to happen after setting the hostname to write the correct config
+# but NOTE: this doesn't seem to work correctly on the initial provisioning
+# chef run. a second chef run is required to converge the variables in the
+# config file correctly
 include_recipe "postfix"
